b272f3a7758446e952148fb8a4281868b9aca062
lisaldofjf <lisaldofjf@gmail.com>
2020-03-23 12:58:32 -0300

    Change Dotenv library to use getenv instead global variable

--- config/app.php
+++ config/app.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types = 1);
 
-use Symfony\Component\Dotenv\Dotenv;
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv->load(dirname(__DIR__) . '/.env');
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();

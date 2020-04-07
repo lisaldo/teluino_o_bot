aa988a77926ba5a36aac9838878e9f2026354beb
lisaldofjf <lisaldofjf@gmail.com>
2020-04-06 21:03:25 -0300

    Change Request to be an interface

--- src/Connection/Request.php
+++ src/Connection/Request.php
@@ -4,19 +4,7 @@ declare(strict_types = 1);
 
 namespace Teluino\Connection;
 
-use GuzzleHttp\Psr7\Request as GuzzleRequest;
-
-class Request
+interface Request
 {
-    private GuzzleRequest $guzzleRequest;
-
-    public function __construct(GuzzleRequest $request)
-    {
-        $this->guzzleRequest = $request;
-    }
-
-    public function get(string $link): string
-    {
-        return '';
-    }
+    public function get(string $link): string;
 }
